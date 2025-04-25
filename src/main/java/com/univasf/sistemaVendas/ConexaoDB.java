bade809b561584cdc5087a98842ddbd2c7e8776e
Gabriel "Snywy" Furtado <gfcleao@gmail.com>
2025-04-25 08:13:28 -0300

    Update ConexaoDB.java

--- src/main/java/com/univasf/sistemaVendas/ConexaoDB.java
+++ src/main/java/com/univasf/sistemaVendas/ConexaoDB.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
