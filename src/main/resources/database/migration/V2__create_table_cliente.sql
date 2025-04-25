446702fc6ce9843c9d9b0cad26d7788ce5484909
Gabriel "Snywy" Furtado <gfcleao@gmail.com>
2025-04-24 22:23:23 -0300

    Remoção do Flyway
    Falta só integrar o create_tables.sql na inicialização.

--- src/main/resources/database/migration/V2__create_table_cliente.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Cliente (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR (255) NOT NULL,
-    email VARCHAR (255) UNIQUE NOT NULL,
-    telefone VARCHAR (20),
-    data_cadastro DATE NOT NULL
-);
---
+++ src/main/resources/database/migration/create_tables.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE IF NOT EXISTS sistemaVendas;
+USE sistema_vendas;
+-- Tabela Cliente
+CREATE TABLE Cliente (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR (255) NOT NULL,
+  email VARCHAR (255) UNIQUE NOT NULL,
+  telefone VARCHAR (20),
+  data_cadastro DATE NOT NULL
+);
+-- Tabela Produtos
+CREATE TABLE Produtos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR (255) NOT NULL,
+  descricao TEXT,
+  preco DECIMAL (10, 2) NOT NULL,
+  estoque INT NOT NULL,
+  data_cadastro DATE NOT NULL
+);
+-- Tabela Vendas
+CREATE TABLE Vendas (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  cliente_id INT NOT NULL,
+  data_venda DATETIME NOT NULL,
+  valor_total DECIMAL (10, 2) NOT NULL,
+  FOREIGN KEY (cliente_id) REFERENCES Cliente (id)
+);
+-- Tabela ItensVenda
+CREATE TABLE ItensVenda (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  venda_id INT NOT NULL,
+  produto_id INT NOT NULL,
+  quantidade INT NOT NULL,
+  preco_unitario DECIMAL (10, 2) NOT NULL,
+  FOREIGN KEY (venda_id) REFERENCES Vendas (id),
+  FOREIGN KEY (produto_id) REFERENCES Produtos (id)
+);
